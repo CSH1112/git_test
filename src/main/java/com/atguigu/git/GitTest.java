e4204b387b27e9cf2f0511209843f4360eb91e92
CSH <1508722171@qq.com>
2022-12-27 18:16:52 +0800

    master test

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -1,10 +1,14 @@
 package com.atguigu.git;
 
+/**
+ * @author CSH
+ */
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("123");
         System.out.println("123");
         System.out.println("123");
         System.out.println("4");
+        System.out.println("master");
     }
 }
